92d4a5c3708fd94e3561e1f4c59c4ddcf3688d88
Filip Troníček <filip.tronicek@seznam.cz>
2020-08-11 22:49:33 +0200

    Formatting

--- index.js
+++ index.js
@@ -1,69 +1,70 @@
-var contentDiv = document.getElementById('contentpage');
-var piDiv = document.getElementById('pi');
+var contentDiv = document.getElementById("contentpage");
+var piDiv = document.getElementById("pi");
 var piTimer;
 var piLength;
 var piCountLength = 0;
-var currNumTxt = '';
-var currPosTxt = '';
-var file = '250ts.txt';
-$('#conentpage').hide();
+var currNumTxt = "";
+var currPosTxt = "";
+var file = "250ts.txt";
+$("#conentpage").hide();
 
-var sfx = new Audio('sound/blob.mp3');
+var sfx = new Audio("sound/blob.mp3");
 
+$(document).on("keypress", function (e) {
+  console.log("Key pressed");
+  if (e.which == 13) {
+    console.log("Enter pressed");
+    if (document.getElementById("input").value <= 250000) {
+      file = "250ts.txt";
+    } else if (document.getElementById("input").value <= 500000) {
+      file = "500ts.txt";
+    } else if (document.getElementById("input").value <= 1000000) {
+      file = "milion.txt";
+    } else {
+      file = "25mil.txt";
+    }
+    $.get("/txt/" + file, {}, function (data) {
+      console.log("Got the file");
 
-$(document).on('keypress', function(e) {
-	console.log('Key pressed');
-	if (e.which == 13) {
-		console.log('Enter pressed');
-		if(document.getElementById('input').value <= 250000) {
-			file = "250ts.txt";
-		} else if (document.getElementById('input').value <= 500000) {
-			file = "500ts.txt";
-		} else if (document.getElementById('input').value <= 1000000) {
-			file = "milion.txt";
-		} else {
-			file = "25mil.txt";	
-		}
-		$.get("/txt/"+	file, {}, function(data) {
-			console.log('Got the file');
-
-			console.log('Showing ' + document.getElementById('input').value + ' digits of pi');
-			console.log(data);
-			$('#inputpage').hide();
-			$('#conentpage').show();
-			Main(data, (document.getElementById('input').value)+2);
-		});
-	}
+      console.log(
+        "Showing " + document.getElementById("input").value + " digits of pi"
+      );
+      console.log(data);
+      $("#inputpage").hide();
+      $("#conentpage").show();
+      Main(data, document.getElementById("input").value + 2);
+    });
+  }
 });
 function wait(ms) {
-	var d = new Date();
-	var d2 = null;
-	do {
-		d2 = new Date();
-	} while (d2 - d < ms);
+  var d = new Date();
+  var d2 = null;
+  do {
+    d2 = new Date();
+  } while (d2 - d < ms);
 }
 
 function Main(content, count) {
-	console.log('shiiit ' + count);
-	var piray = content.split('');
-	console.log(piray);
-	piLength = piray.length;
+  console.log("shiiit " + count);
+  var piray = content.split("");
+  console.log(piray);
+  piLength = piray.length;
 
-	ShowPime();
-	function ShowPime() {
-		if (document.getElementById('input').value > currPosTxt) {
-			if (piCountLength < piLength) {
-				if (piCountLength > -1) {
-					currNumTxt = piray[piCountLength];
-					currPosTxt = piCountLength + 1;
-					sfx.play();
-				}
-				piCountLength++;
-			}
-		}
-		document.getElementById('pi').innerHTML = "Position: "+currPosTxt;
-		document.getElementById('pos').innerHTML = currNumTxt;
+  ShowPime();
+  function ShowPime() {
+    if (document.getElementById("input").value > currPosTxt) {
+      if (piCountLength < piLength) {
+        if (piCountLength > -1) {
+          currNumTxt = piray[piCountLength];
+          currPosTxt = piCountLength + 1;
+          sfx.play();
+        }
+        piCountLength++;
+      }
+    }
+    document.getElementById("pi").innerHTML = "Position: " + currPosTxt;
+    document.getElementById("pos").innerHTML = currNumTxt;
 
-		setTimeout(ShowPime, 800);
-	}
+    setTimeout(ShowPime, 800);
+  }
 }
